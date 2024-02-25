f7d2541b2386f94c9708fa18c6f110620b6cefcf
richfjiq <rfjiq1986@gmail.com>
2024-02-24 20:28:47 -0600

    chore: adds client component in server componenet and install Monaco Editor

--- src/app/snippets/[id]/edit/page.tsx
+++ src/app/snippets/[id]/edit/page.tsx
@@ -1,12 +1,27 @@
+import { notFound } from 'next/navigation';
+
+import { db } from '@/db';
+import SnippetEditForm from '@/components/snippet-edit-form';
+
 interface SnippetEditPageProps {
   params: {
     id: string;
   };
 }
 
-const SnippetEditPage = (props: SnippetEditPageProps) => {
+const SnippetEditPage = async (props: SnippetEditPageProps) => {
   const id = parseInt(props.params.id);
-  return <div>Editing snippet with id {id}</div>;
+  const snippet = await db.snippet.findFirst({ where: { id } });
+
+  if (!snippet) {
+    return notFound();
+  }
+
+  return (
+    <div>
+      <SnippetEditForm snippet={snippet} />
+    </div>
+  );
 };
 
 export default SnippetEditPage;
---
+++ src/components/snippet-edit-form.tsx
@@ -0,0 +1,32 @@
+'use client';
+import { Editor } from '@monaco-editor/react';
+
+import type { Snippet } from '@prisma/client';
+import { useState } from 'react';
+
+interface SnippetEditFormProps {
+  snippet: Snippet;
+}
+
+const SnippetEditForm = ({ snippet }: SnippetEditFormProps) => {
+  const [code, setCode] = useState(snippet.code);
+
+  const handleEditorChange = (value: string = '') => {
+    setCode(value);
+  };
+
+  return (
+    <div>
+      <Editor
+        height="40vh"
+        theme="vs-dark"
+        language="javascript"
+        defaultValue={snippet.code}
+        options={{ minimap: { enabled: false } }}
+        onChange={handleEditorChange}
+      />
+    </div>
+  );
+};
+
+export default SnippetEditForm;
